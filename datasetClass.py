be3ac6fa7ffde1dc19f0b8b6121231e803bc3b0f
username-df <usernamedf@outlook.com>
2024-10-24 03:48:03 -0400

    created custom dataset class

--- datasetClass.py
+++ datasetClass.py
@@ -1,18 +1,20 @@
 import os
 import torch
-import torchvision.transforms as tr
 from torch.utils.data import Dataset
 from PIL import Image
+from random import randint
 
 class TigerDataset(Dataset):
-    def __init__(self, input_dir, target_dir, transform=None):
+    def __init__(self, input_dir, target_dir, transform_input=None, transform_target=None):
         self.input_dir = input_dir
         self.target_dir = target_dir
-        self.transform = transform
+
+        self.tf_input = transform_input
+        self.tf_target = transform_target
 
         file_nums = [int(f[0: f.index('.')]) for f in os.listdir(input_dir)]
         file_nums = sorted(file_nums)
-        self.file_names = [f"{num}.jpeg" for num in file_nums]
+        self.file_names = [f"{num}.jpg" if os.path.exists(f"{input_dir}/{num}.jpg") else f"{num}.jpeg" for num in file_nums]
 
     def __len__(self):
         return len(self.file_names)
@@ -24,8 +26,14 @@ class TigerDataset(Dataset):
         input_img = Image.open(input_path).convert('RGB')
         target_img = Image.open(target_path).convert('L')
 
-        if self.transform:
-            input_img = self.transform(input_img)
-            target_img = self.transform(target_img)
+
+        if self.tf_input and self.tf_target:
+            rand = randint(0, 10000)
+            
+            torch.manual_seed(rand)
+            input_img = self.tf_input(input_img)
+
+            torch.manual_seed(rand)
+            target_img = self.tf_target(target_img)
 
         return (input_img, target_img)

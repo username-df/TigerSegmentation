039f43dfaada5b4066ea31abb73ff3227c29d2e4
username-df <usernamedf@outlook.com>
2024-11-03 15:53:41 -0500

    Now using 0 for target and 1 for background

--- customDataset.py
+++ customDataset.py
@@ -2,19 +2,36 @@ import torchvision.transforms as transforms
 from datasetClass import TigerDataset
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
+from PIL import Image
 
 BATCH_SIZE = 8
 
+class PadToSquare:
+    def __call__(self, img: Image):
+        width, height = img.size
+        target_size = 572
+        
+        new_img = Image.new("RGB", (target_size, target_size), (0, 0, 0))
+  
+        x_offset = (target_size - width) // 2
+        y_offset = (target_size - height) // 2
+        
+        new_img.paste(img, (x_offset, y_offset))
+        
+        return new_img
+        
 class adjust_pixels:
     def __call__(self, img):
         img[img <= 0.30] = 0
         img[img > 0.30] = 1
+        img = 1 - img
         return img   
     
 tf_input = transforms.Compose([
     transforms.RandomRotation(36),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomResizedCrop((572,572)),
+    transforms.RandomResizedCrop((388,388)),
+    PadToSquare(),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.40497827529907227, 0.3686119616031647, 0.29055872559547424], 
                         std=[0.25618886947631836, 0.23313170671463013, 0.2274409383535385])

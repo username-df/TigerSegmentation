72a059bce35e2310dfc9ddee90f9f6970ff07abd
username-df <usernamedf@outlook.com>
2024-11-02 21:08:32 -0400

    combined resize and padtosquare into one function changed batch size to 8 to use gradient accumulation

--- customDataset.py
+++ customDataset.py
@@ -1,54 +1,36 @@
 import torchvision.transforms as transforms
 from datasetClass import TigerDataset
 from torch.utils.data import DataLoader
-from PIL import Image
+from PIL import Image, ImageOps
 from sklearn.model_selection import train_test_split
 
-BATCH_SIZE = 32
+BATCH_SIZE = 8
 
-class ImgResize:
-    def __call__(self, img: Image, output_size=(572,572)):
-        image = img
+class ZeroPadding:
+    def __init__(self, mode, target_size):
+        self.mode = mode.upper()
+        self.target_size = target_size
 
-        original_width, original_height = image.size
+    def __call__(self, image):
+        image = ImageOps.contain(image, self.target_size)
         
-        aspect_ratio = original_width / original_height
+        zeros = Image.new(self.mode, self.target_size)
 
-        target_width, target_height = output_size
-        if target_width / target_height > aspect_ratio:
-            target_width = int(target_height * aspect_ratio)
-        else:
-            target_height = int(target_width / aspect_ratio)
+        x_offset = (self.target_size[0] - image.width) // 2
+        y_offset = (self.target_size[1] - image.height) // 2
 
-        resized_image = image.resize((target_width, target_height), Image.LANCZOS)
+        zeros.paste(image, (x_offset, y_offset))
+            
+        return zeros
 
-        return resized_image
-
-class PadToSquare:
-    def __call__(self, img: Image):
-
-        width, height = img.size
- 
-        target_size = max(width, height)
-
-        new_img = Image.new("RGB", (target_size, target_size), (0, 0, 0))
-  
-        x_offset = (target_size - width) // 2
-        y_offset = (target_size - height) // 2
-        
-        new_img.paste(img, (x_offset, y_offset))
-        
-        return new_img
-    
 class adjust_pixels:
     def __call__(self, img):
         img[img <= 0.30] = 0
-        img[img >= 0.60] = 1
+        img[img > 0.30] = 1
         return img   
     
 tf_input = transforms.Compose([
-    ImgResize(),
-    PadToSquare(),
+    ZeroPadding(mode="RGB", target_size=(572,572)), 
     transforms.RandomRotation(36),
     transforms.RandomHorizontalFlip(),
     transforms.RandomResizedCrop((572,572)),
@@ -58,15 +40,12 @@ tf_input = transforms.Compose([
 ])
 
 tf_target = transforms.Compose([
-    ImgResize(),
-    PadToSquare(),
+    ZeroPadding(mode="L", target_size=(388,388)), 
     transforms.RandomRotation(36),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomResizedCrop((572,572)),
+    transforms.RandomResizedCrop((388, 388)),
     transforms.ToTensor(),
-    adjust_pixels(),
-    transforms.Normalize(mean=[0.2799879014492035, 0.2799879014492035, 0.2799879014492035], 
-                        std=[0.44774481654167175, 0.44774481654167175, 0.44774481654167175])
+    adjust_pixels()
 ])
 
 dataset = TigerDataset(input_dir="imageData\\inputs", target_dir="imageData\\targets",

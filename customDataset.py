fe6c21f0e51874bc285e888eba32f12cbc858900
username-df <usernamedf@outlook.com>
2024-11-03 07:27:41 -0500

    just going to use resizing from resizedcrop

--- customDataset.py
+++ customDataset.py
@@ -1,27 +1,9 @@
 import torchvision.transforms as transforms
 from datasetClass import TigerDataset
 from torch.utils.data import DataLoader
-from PIL import Image, ImageOps
 from sklearn.model_selection import train_test_split
 
 BATCH_SIZE = 8
-
-class ZeroPadding:
-    def __init__(self, mode, target_size):
-        self.mode = mode.upper()
-        self.target_size = target_size
-
-    def __call__(self, image):
-        image = ImageOps.contain(image, self.target_size)
-        
-        zeros = Image.new(self.mode, self.target_size)
-
-        x_offset = (self.target_size[0] - image.width) // 2
-        y_offset = (self.target_size[1] - image.height) // 2
-
-        zeros.paste(image, (x_offset, y_offset))
-            
-        return zeros
 
 class adjust_pixels:
     def __call__(self, img):
@@ -30,7 +12,6 @@ class adjust_pixels:
         return img   
     
 tf_input = transforms.Compose([
-    ZeroPadding(mode="RGB", target_size=(572,572)), 
     transforms.RandomRotation(36),
     transforms.RandomHorizontalFlip(),
     transforms.RandomResizedCrop((572,572)),
@@ -40,12 +21,11 @@ tf_input = transforms.Compose([
 ])
 
 tf_target = transforms.Compose([
-    ZeroPadding(mode="L", target_size=(388,388)), 
     transforms.RandomRotation(36),
     transforms.RandomHorizontalFlip(),
     transforms.RandomResizedCrop((388, 388)),
     transforms.ToTensor(),
-    adjust_pixels()
+    adjust_pixels(),
 ])
 
 dataset = TigerDataset(input_dir="imageData\\inputs", target_dir="imageData\\targets",
